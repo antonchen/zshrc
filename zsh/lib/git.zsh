f782c380420c1a80be66f81a46126a353b4b97da
Anton Chen <contact@antonchen.com>
2018-05-29 14:42:03 +0800

    fixed _git_branch_name

--- zsh/lib/git.zsh
+++ zsh/lib/git.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 # File: git.zsh
 # Version: 0.1
-# Last Modified: 2018-03-23 23:25:33
+# Last Modified: 2018-05-29 14:41:53
 # Author: Anton Chen
 # Email: contact@antonchen.com
 _check_git()
@@ -12,15 +12,28 @@ _check_git()
 
 _git_repo_name()
 {
-    gitDir=$(git rev-parse --git-dir 2> /dev/null)
-    if [[ "X$gitDir" == "X.git" ]]; then
+    local GIT_DIR
+    GIT_DIR=$(git rev-parse --git-dir 2> /dev/null)
+    if [[ "$GIT_DIR" == ".git" ]]; then
         echo -n $(basename $(pwd))
-    elif [[ "X$gitDir" != "X" ]]; then
-        echo -n $(dirname $gitDir | xargs basename)
+    elif [[ -n $GIT_DIR ]]; then
+        echo -n $(dirname $GIT_DIR | xargs basename)
     fi
 }
 
 _git_branch_name()
 {
-    git name-rev --name-only --no-undefined --always HEAD 2>/dev/null
+    local B_TEMP B T H
+    B_TEMP=$(git symbolic-ref HEAD 2>/dev/null)
+    B=${B_TEMP##refs/heads/}
+    T=$(git describe --exact-match --tags $(git log -n1 --pretty='%h') 2>/dev/null)
+    H=$(git rev-parse --short HEAD 2>/dev/null)
+
+    if [[ -n $B ]]; then
+        echo $B
+    elif [[ -n $T ]]; then
+        echo "tags/$T"
+    elif [[ -n $H ]]; then
+        echo $H
+    fi
 }
