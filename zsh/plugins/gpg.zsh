ed9f00bd6230fd7016917fe53549b83e49e12316
Anton Chen <contact@antonchen.com>
2018-10-18 14:05:52 +0800

    disable jobs output

--- zsh/plugins/gpg.zsh
+++ zsh/plugins/gpg.zsh
@@ -3,7 +3,7 @@
 # Author: Anton Chen
 # Version: 0.1
 # Email: contact@antonchen.com
-# Last Modified: 2018-10-18 13:44:17
+# Last Modified: 2018-10-18 14:01:45
 
 __gpg_is_exists=0
 if command -v gpg > /dev/null 2>&1; then
@@ -13,7 +13,7 @@ fi
 if [ $__gpg_is_exists -eq 1 ] && [[ "$(uname)" == "Darwin" ]]; then
     pgrep -q gpg-agent
     if [ $? -ne 0 ]; then
-        gpgconf --launch gpg-agent &
+        nohup gpgconf --launch gpg-agent > /dev/null 2>&1 &!
     fi
     export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
 fi
