8b278a45056bcdc46cbee5ec07cb163b65eb8046
Anton Chen <contact@antonchen.com>
2019-01-08 12:27:45 +0800

    Add alias -s

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 # File: alias.zsh
 # Version: 0.1
-# Last Modified: 2018-10-12 14:30:54
+# Last Modified: 2019-01-08 12:27:12
 # Author: Anton Chen
 # Email: contact@antonchen.com
 
@@ -22,6 +22,14 @@ alias rscp='rsync -P --rsh=ssh'
 alias aria2c='aria2c -c -s 5'
 alias fuck='proxychains4'
 alias g='git'
+
+# 直接输入指定后缀文件名，使用指定命令打开
+alias -s js='vim'
+alias -s txt='vim'
+alias -s gz='tar -xzvf'
+alias -s tgz='tar -xzvf'
+alias -s bz2='tar -xjvf'
+alias -s zip='unzip'
 
 if command -v npm > /dev/null 2>&1; then
     alias cnpm="npm --registry=https://registry.npm.taobao.org --cache=$HOME/.npm/.cache/cnpm --disturl=https://npm.taobao.org/dist --userconfig=$HOME/.cnpmrc"
