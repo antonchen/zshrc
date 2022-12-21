ad19385960f174412808619a3423771b31242cbe
Anton Chen <contact@antonchen.com>
2022-12-21 10:20:35 +0800

    Update macos.zsh

--- zsh/plugins/macos.zsh
+++ zsh/plugins/macos.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 # Brew mirrors
-export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles"
+export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.ustc.edu.cn/brew.git"
 
 vncviewer()
 {
